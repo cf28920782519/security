ca0c4d971acf14e629591480a90ae7e81c169f90
zhiwei <2878117430@qq.com>
2019-04-29 17:11:26 +0800

    逾期未检车辆

--- Poor_stability_1.py
+++ Poor_stability_1.py
@@ -20,7 +20,7 @@ def dangerous_vehicle_type2_query(conn):
                   "QZBFQZ>= to_date('2019-01-01','YYYY-MM-DD')" # 查询2019年1月1日之前的所有报废车的车牌、类型、报废日期
 
     cr.execute(sql_vehilce)
-    query_res = cr.fetchmany(1000)  # 查询结果为元组形式[(数据1),(数据2),(数据3)]
+    query_res = cr.fetchall()  # 查询结果为元组形式[(数据1),(数据2),(数据3)]
     cr.close()
     return query_res
 
@@ -51,7 +51,7 @@ def get_WFSJ_MAX(conn, HPHM, HPZL):
     WFSJ_MAX =[]    # 初始化最晚违法时间的列表
     for i in range(num):
         if WFSJ_MAX_tuple[i][0] == None:    # 如果没查到违法时间，则最晚违法时间设置为1990年1月1日（VEHICLE表中的YXQZ最小为1992年6月8日）
-            WFSJ_MAX.append(datetime.datetime(1990, 1, 1, 1, 1))
+            WFSJ_MAX.append(datetime.datetime(1980, 1, 1, 1, 1))
         else:
             WFSJ_MAX.append(WFSJ_MAX_tuple[i][0])   # 加入SURVEIL表的查询结果
     return WFSJ_MAX
@@ -70,6 +70,36 @@ def get_dangerous_vehicle_t2(YXQZ, WFSJ_MAX, HPHM, HPZL):
             else: continue
         return dangerous_HPHM, dangerous_HPZL, WF_time_interval
 
+# 生成车牌号牌的危险类型标识的list（对应Dangerous_Vehicles表的Danger_Type字段）
+def Danger_type_list(dangerous_HPHM,danger_tp):
+    return [str(danger_tp)]*len(dangerous_HPHM)
+
+# 计算结果整理成批量插入SQL数据库所需要的格式
+def Trans_DataFrame(dangerous_HPHM, dangerous_HPZL, WF_time_interval, Danger_Type):         # 这3个list长度一致，等于所统计的车辆数
+    a = [dangerous_HPHM, dangerous_HPZL, WF_time_interval, Danger_Type]
+    return list(zip(*a))
+
+# 批量插入数据库
+def Insert_db(conn, result):
+    if conn == None:
+        conn = get_connection()
+    cr = conn.cursor()
+    # print('diaoyong')
+
+    sql = "INSERT INTO DANGEROUS_VEHICLES(HPHM, HPZL, WF_Time_Interval, Danger_Type) VALUES (:1, :2, :3, :4)"
+
+    try:
+        cr.executemany(sql, result)
+        conn.commit()
+        print('insert successfully!')
+    except:
+        conn.rollback()
+
+    # 关闭游标、关闭数据库连接
+    free(conn, cr)
+    return 0
+
+
 
 if __name__ == '__main__':
     starttime = datetime.datetime.now()  # 统计程序的开始时刻
@@ -87,3 +117,12 @@ if __name__ == '__main__':
     print(dangerous_HPHM)
     print(len(dangerous_HPHM))
     print(WF_time_interval)
+    Danger_Tp = Danger_type_list(dangerous_HPHM, 'E')   # 表明车辆的危险标签是逾期未年检expire
+    result = Trans_DataFrame(dangerous_HPHM, dangerous_HPZL, WF_time_interval, Danger_Tp)
+    print(result)
+    print(len(result))
+    Insert_db(conn, result)
+
+    endtime = datetime.datetime.now()
+    print("the program runs : %d s" % (endtime - starttime).seconds)
+
